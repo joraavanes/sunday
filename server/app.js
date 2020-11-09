ecc467246ed02aabc7a82f601da6fc5e69167520
joraavanes <jora_a@outlook.com>
2020-11-09 19:42:23 +0330

    database connection added - dotenv config - chalk

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+const {log} = console;
+
+require('dotenv').config();
 
 const app = express();
 
@@ -9,4 +14,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => res.sendFile(indexPath));
 
-app.listen(3000, () => console.log('App is listening on port 3000'));
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, poolSize: 100})
+    .then(result => {
+        app.listen(3000, () => log(chalk.bold.bgRgb(11, 153, 3)('App is listening on http://localhost:3000')));
+    })
+    .catch(err => {
+        console.log(err);
+    });
